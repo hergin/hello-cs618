3075f2ee539bc0d092e1e914742c5e7e9672d5ef
Huseyin Ergin <mersinus@gmail.com>
2025-07-25 13:25:00 +0000

    chore: intro sockets

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,9 @@ import { userRoutes } from './routes/users.js'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
+
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
@@ -15,4 +18,20 @@ app.get('/', (req, res) => {
   res.send('Hello from Express Nodemon!')
 })
 
-export { app }
+const server = createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+})
+
+io.on('connection', (socket) => {
+  console.log('user connected:', socket.id)
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected:', socket.id)
+  })
+})
+
+export { server as app }
--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,18 @@ import { Blog } from './pages/Blog.jsx'
 import { Signup } from './pages/Signup.jsx'
 import { Login } from './pages/Login.jsx'
 import { AuthContextProvider } from './contexts/AuthContext.jsx'
-
+import { io } from 'socket.io-client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
+const socket = io(import.meta.env.VITE_SOCKET_HOST)
 const queryClient = new QueryClient()
+
+socket.on('connect', () => {
+  console.log('connected to socket.io as', socket.id)
+})
+
+socket.on('connect_error', (err) => {
+  console.error('socket.io connect error:', err)
+})
 
 const router = createBrowserRouter([
   {
