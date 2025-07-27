72f5f19e09b24cea35b19e803b2e67e7a4698a4e
Huseyin Ergin <mersinus@gmail.com>
2025-07-27 05:49:12 +0000

    feat: sockets implemented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
 import { Link } from 'react-router-dom'
 import { jwtDecode } from 'jwt-decode'
 import { User } from './User.jsx'
+import { useSocket } from '../contexts/SocketIOContext.jsx'
 
 import { useAuth } from '../contexts/AuthContext.jsx'
 
 export function Header() {
   const [token, setToken] = useAuth()
+  const { socket } = useSocket()
+
+  const handleLogout = () => {
+    socket.disconnect()
+    setToken(null)
+  }
 
   if (token) {
     const { sub } = jwtDecode(token)
@@ -14,7 +21,7 @@ export function Header() {
         <h1>Welcome to My Blog!</h1>
         Logged in as <User id={sub} />
         <br />
-        <button onClick={() => setToken(null)}>Logout</button>
+        <button onClick={handleLogout}>Logout</button>
       </div>
     )
   }
---
+++ src/components/Status.jsx
@@ -0,0 +1,10 @@
+import { useSocket } from '../contexts/SocketIOContext.jsx'
+
+export function Status() {
+  const { status, error } = useSocket()
+  return (
+    <div>
+      Socket status: <b>{status}</b> {error && <i> - {error.message}</i>}
+    </div>
+  )
+}
